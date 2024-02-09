b056d72ea081feb50b3bc06089090b87e3a2b4e2
ITACHI <omadbeksalomov@Omadbeks-MacBook-Air.local>
2024-02-09 16:03:05 +0500

    news commit

--- news/settings.py
+++ news/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wl9t6b(z&y8pu3=rvst79(&d3_4$-ip&i(7o5ey24y@-3#dlf='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -127,7 +127,7 @@ STATICFILES_FINDERS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/home/djangone/django-news-project.uz/django/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
